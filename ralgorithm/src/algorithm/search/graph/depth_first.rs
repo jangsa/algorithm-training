1063d0b1228d6854d280af0c0820b1ab059add22
jangsa <gute.ist.wieder.tot@gmail.com>
2019-02-03 21:55:53 +0900

    Rust 2018 allows to pass ownership in recursive call

--- ralgorithm/src/algorithm/search/graph/depth_first.rs
+++ ralgorithm/src/algorithm/search/graph/depth_first.rs
@@ -15,11 +15,12 @@ pub fn _search(
     mut field: HashMap<Vertex<i32>, Vec<Vertex<i32>>>,
     goal: &Vertex<i32>,
 ) -> Vec<Vertex<i32>> {
-    let _curr = match field.get_mut(&curr) {
+    /* the following works on Rust 2018 */
+    match field.get_mut(&curr) {
         // when reached goal
         _ if *curr == *goal => {
             acc.push(*curr);
-            return acc;
+            acc
         }
 
         // when vertices found
@@ -27,46 +28,17 @@ pub fn _search(
             if let Some(next) = ns.pop() {
                 // go to next depth
                 acc.push(*curr);
-                next
+                _search(&next, acc, field, goal) // to be fixed
             } else if let Some(prev) = acc.pop() {
                 // backtrack
-                prev
+                _search(&prev, acc, field, goal) // to be fixed
             } else {
                 // no answer
-                return vec![];
+                vec![]
             }
         }
 
         // when next is not registered
-        None => return vec![],
-    };
-
-    _search(&_curr, acc, field, goal)
-
-    /* the following works on Rust 2018 */
-    // match field.get_mut(&curr) {
-    //     // when reached goal
-    //     _ if *curr == *goal => {
-    //         acc.push(*curr);
-    //         acc
-    //     }
-
-    //     // when vertices found
-    //     Some(ns) => {
-    //         if let Some(next) = ns.pop() {
-    //             // go to next depth
-    //             acc.push(*curr);
-    //             _search(&next, acc, field, goal) // to be fixed
-    //         } else if let Some(prev) = acc.pop() {
-    //             // backtrack
-    //             _search(&prev, acc, field, goal) // to be fixed
-    //         } else {
-    //             // no answer
-    //             vec![]
-    //         }
-    //     }
-
-    //     // when next is not registered
-    //     None => vec![],
-    // }
+        None => vec![],
+    }
 }
